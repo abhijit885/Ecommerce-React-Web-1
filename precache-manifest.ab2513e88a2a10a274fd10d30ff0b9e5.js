675572c9f0bd907b5e2de941fe077fd07256a735
abhijit885 <abhijitsaha885@gmail.com>
2020-12-13 19:49:04 +0530

    Updates

---
+++ precache-manifest.ab2513e88a2a10a274fd10d30ff0b9e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d6ace1753418512ecea3e92ecc1465",
+    "url": "/Ecommerce-React-Web-1/index.html"
+  },
+  {
+    "revision": "7d4040dd70715d631ad0",
+    "url": "/Ecommerce-React-Web-1/static/css/main.706448ca.chunk.css"
+  },
+  {
+    "revision": "019e6ce9e4a826ae94f1",
+    "url": "/Ecommerce-React-Web-1/static/js/2.b85d08bb.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/Ecommerce-React-Web-1/static/js/2.b85d08bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4040dd70715d631ad0",
+    "url": "/Ecommerce-React-Web-1/static/js/main.5e7fd8c0.chunk.js"
+  },
+  {
+    "revision": "9c00c38f6c5964ece4b0",
+    "url": "/Ecommerce-React-Web-1/static/js/runtime-main.18ecb59c.js"
+  }
+]);
